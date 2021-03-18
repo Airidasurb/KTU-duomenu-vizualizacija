0cb729fa96324375a9c243410b317195ac94929f
airidasurb <airidas.urb1@gmail.com>
2021-03-18 11:19:28 +0200

    fin

--- laboratorinis/R/lab.R
+++ laboratorinis/R/lab.R
@@ -3,30 +3,37 @@ library(ggplot2)
 data <- read_csv("lab_sodra.csv")
 summary(data)
 
-data%>%
-  filter(ecoActCode == 452000)%>%
+datafiltered <- data %>%
+  filter(ecoActCode == 452000)
+
+datafiltered%>%
   ggplot(aes(x = avgWage)) +
   geom_histogram(bins = 100)
 
 
-data1 <- data %>%
-  filter(ecoActCode == 452000) %>%
+data1 <- datafiltered %>%
   group_by(name) %>%
  summarise(avg = mean(avgWage)) %>%
   arrange(desc(avg))
 
-data3 <- merge(data, data1)
+data2 <- merge(datafiltered, data1)
 
 
-data3%>%
+data2%>%
   arrange(desc(avg))%>%
   head(60)%>%
 ggplot(aes(x = month, y = avgWage, group = name)) + theme_minimal()+
-  geom_line(aes(colour = as.factor(name))) + theme(axis.text.x=element_blank())
+  geom_line(aes(colour = name)) + geom_point(aes(color = name)) +
+  theme(axis.text.x=element_blank())
 
 
-
-
-
+data2 %>%
+  arrange(desc(avg))%>%
+  head(60)%>%
+  group_by(name)%>%
+  slice_max(numInsured, with_ties = FALSE)%>%
+  head(5)%>%
+ggplot(aes(x = reorder(name,-numInsured), y = numInsured)) + geom_col(aes(fill = name)) + 
+  theme(axis.text.x=element_blank()) + xlab('name')
 
 
---
+++ laboratorinis/app/app.R
@@ -0,0 +1,33 @@
+
+library(shiny)
+library(tidyverse)
+
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      selectizeInput(inputId = "imones_kodas", label = "Imones kodas", choices = NULL, selected = NULL)
+    ),
+    mainPanel(tabsetPanel(
+      tabPanel("grafikas", plotOutput("plot")),
+      tabPanel("lentele", tableOutput("table"))
+    )
+    )
+  )
+)
+server <- function(input, output, session) {
+  data <- read_csv("../data/lab_sodra.csv")
+  updateSelectizeInput(session, "imones_kodas", choices = datafiltered$name, server = TRUE)
+  
+  output$table <- renderTable(
+    datafiltered %>%
+      filter(name == input$imones_kodas) , digits = 0
+  )
+  
+  output$plot <- renderPlot(
+    datafiltered %>%
+      filter(name == input$imones_kodas) %>%
+      ggplot(aes(x = month, y = avgWage)) +
+      geom_line()      
+  )
+}
+shinyApp(ui, server)
